7dd941acc37d775e92b0a691f1ac3545e6787e59
doku <student@ca308-7.local>
2016-02-04 14:07:14 -0800

    new img

--- example2/js/script.js
+++ example2/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ // Detects of the page is "ready"
-    
+
     // Panorama effect
     $(".panorama").panorama_viewer({
         repeat: true,
@@ -8,15 +8,27 @@ $(document).ready(function(){ // Detects of the page is "ready"
         easing: "ease-out",
         overlay: true
     });
-    
+
     // Assigns a click function to anything with the the ID #pan-btn-a
     $("#pan-btn-a").click(function() {
-        
+
         // Changes an attribute of an image that has the ID #pan-image
         $('#pan-image').attr('src','images/panorama1.jpg');
-    })
-    
+    });
+
      $("#pan-btn-b").click(function() {
         $('#pan-image').attr('src','images/panorama2.jpg');
-    })
+    });
+    var panImage = "images/panorama3.jpg";
+
+    $('#pan-btn-c').click(function() {
+      $('#pan-image').attr('src', panImage);
+
+    });
+    $('#pan-btn-d').click(function() {
+      $('#pan-image').attr('src', "images/panorama4.jpg");
+
+    });
+
+
 });
