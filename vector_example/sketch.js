6e6a0161c42924da8fd89337304c6018b2f1db08
doku <jason@jasonwang.us>
2016-03-17 13:17:37 -0700

    Update sketch.js

--- vector_example/sketch.js
+++ vector_example/sketch.js
@@ -1,11 +1,24 @@
 
-function setup(){
+var vLocation;
+var vSpeed;
 
+function setup(){
+  createCanvas(800, 600);
+  background(255);
+  
+  vLocation = createVector(100,100);
+  vSpeed = createVector(1,3.3);
 }
 
 
 function draw(){
   ellipse(50,50,80,80);
+  
+  vLocation.add(vSpeed);
+  
+
+
+
 
 }
 
