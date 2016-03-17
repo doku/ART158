5fa336bfc570163b53b0ddc66588070c982333f2
doku <jason@jasonwang.us>
2016-03-17 13:22:31 -0700

    Update sketch.js

--- vector_example/sketch.js
+++ vector_example/sketch.js
@@ -12,11 +12,18 @@ function setup(){
 
 
 function draw(){
-  ellipse(50,50,80,80);
+  background(255);
+  ellipse(vLocation.x, vLocation.y,80,80);
   
   vLocation.add(vSpeed);
   
-
+  if((vLocation.x > width) || (vLocacation.x < 0)){
+    vSpeed.x = vSpeed.x * -1;
+  }
+  if((vlocation.y > height) || (vLocation.y < 0)){
+    vSpeed.y = vSpeed.y * -1;
+  
+  }
 
 
 
